c69ae7fa2310f4696e34d1ea6cbb4d97d12184af
Polig0nas <t.polegenko@gmail.com>
2023-03-26 17:10:12 +0300

    Form

--- src/pages/FormPage.js
+++ src/pages/FormPage.js
@@ -1,87 +1,112 @@
 import * as React from "react";
 import {Helmet} from 'react-helmet-async';
-import {Button, Container, Stack, Typography} from '@mui/material';
-import Iconify from '../components/iconify';
+import {Button, Container, MenuItem, Select, Stack, TextField, Typography} from '@mui/material';
 import {Field, Form, Formik} from "formik";
 import Label from "../components/label";
 import formSchema from "../schemas";
 
 export default function FormPage() {
-  return (
-    <>
-      <Helmet>
-        <title> Dashboard: Form</title>
-      </Helmet>
-
-      <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-          <Typography variant="h4" gutterBottom>
-           Form
-          </Typography>
-          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
-            New Post
-          </Button>
-        </Stack>
-      </Container>
+    return (
         <>
-            <Formik
-                initialValues={{
-                    make: '',
-                    model: '',
-                    yearOfMade: '',
-                    bodyType: '',
-                    fuelType: '',
-                    licencePlate: '',
-                }}
-                validationSchema={formSchema}
+            <Helmet>
+                <title> Dashboard: Form</title>
+            </Helmet>
 
-                onSubmit={async (values) => {
-                    await new Promise((r) => setTimeout(r, 500));
-                    alert(JSON.stringify(values, null, 2));
-                }}
-            >
-                <Form>
-                    <Label htmlFor="make">Make</Label> <br/>
-                    <Field id="make"
-                           name="make"
-                           placeholder="Make"
-                           />
-                    <br/>
+            <Container>
+                <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+                    <Typography variant="h4" gutterBottom>
+                        Form
+                    </Typography>
+                </Stack>
+            </Container>
+            <div align="center">
+                <Formik
+                    initialValues={{
+                        make: '',
+                        model: '',
+                        yearOfMade: '',
+                        bodyType: '',
+                        fuelType: '',
+                        licencePlate: '',
+                    }}
+                    validationSchema={formSchema}
 
-                    <Label htmlFor="model">Model</Label><br/>
-                    <Field id="model"
-                           name="model"
-                           placeholder="Model"/>
-                    <br/>
+                    onSubmit={async (values) => {
+                        await new Promise((r) => setTimeout(r, 500));
+                        alert(JSON.stringify(values, null, 2));
+                    }}>
+                    {(props) => {
+                        return (
+                            <Form>
+                                <Label htmlFor="make">Make</Label> <br/>
+                                <Field id="make"
+                                       type="text"
+                                       name="make"
+                                       placeholder="Make"
+                                       error={!!props.errors.make && props.touched.make}
+                                       helperText={props.touched.make && props.errors["make"]}
+                                       as={TextField}
+                                />
+                                <br/>
 
-                    <Label htmlFor="yearOfMade">Year</Label><br/>
-                    <Field id="yearOfMade"
-                           name="yearOfMade"
-                           placeholder="Year"/>
-                    <br/>
+                                <Label htmlFor="model">Model</Label><br/>
+                                <Field id="model"
+                                       type="text"
+                                       name="model"
+                                       placeholder="Model"
+                                       error={!!props.errors.model && props.touched.model}
+                                       helperText={props.touched.model && props.errors["model"]}
+                                       as={TextField}/>
+                                <br/>
 
-                    <Label htmlFor="bodyType">Body</Label><br/>
-                    <Field id="bodyType"
-                           name="bodyType"
-                           placeholder="Body"/>
-                    <br/>
+                                <Label htmlFor="yearOfMade">Year</Label><br/>
+                                <Field id="yearOfMade"
+                                       name="yearOfMade"
+                                       placeholder="Year"
+                                       error={!!props.errors.yearOfMade && props.touched.yearOfMade}
+                                       helperText={props.touched.yearOfMade && props.errors["yearOfMade"]}
+                                       as={TextField}/>
+                                <br/>
+                                <Label htmlFor="bodyType">Body</Label><br/>
+                                <Field id="bodyType"
+                                       name="bodyType"
+                                       placeholder="Body"
+                                       error={!!props.errors.bodyType && props.touched.bodyType}
+                                       helperText={props.touched.bodyType && props.errors["bodyType"]}
+                                       as={Select}
+                                >
+                                    <MenuItem value="sedan">Sedan</MenuItem>
+                                    <MenuItem value="wagon">Wagon</MenuItem>
+                                </Field><br/>
+                                <Label htmlFor="fuelType">Fuel</Label><br/>
+                                <Field id="fuelType"
+                                       name="fuelType"
+                                       placeholder="Fuel"
+                                       error={!!props.errors.fuelType && props.touched.fuelType}
+                                       helperText={props.touched.fuelType && props.errors["fuelType"]}
+                                       as={Select}
+                                >
+                                    <MenuItem value="gasoline">Gasoline</MenuItem>
+                                    <MenuItem value="diesel">Diesel</MenuItem>
+                                </Field>
+                                <br/>
 
-                    <Label htmlFor="fuelType">Fuel</Label><br/>
-                    <Field id="fuelType"
-                           name="fuelType"
-                           placeholder="Fuel"/>
-                    <br/>
-
-                    <Label htmlFor="licencePlate">Licence Plate</Label><br/>
-                    <Field id="licencePlate"
-                           name="licencePlate"
-                           placeholder="Number"/>
-                    <br/>
-
-                    <Button type="submit">Submit</Button>
-                </Form>
-            </Formik>
+                                <Label>Licence Plate</Label><br/>
+                                <Field id="licencePlate"
+                                       type="text"
+                                       name="licencePlate"
+                                       placeholder="Number"
+                                       error={!!props.errors.licencePlate && props.touched.licencePlate}
+                                       helperText={props.touched.licencePlate && props.errors["licencePlate"]}
+                                       as={TextField}/>
+                                <br/><br/>
+                                <Button variant="contained" type="submit">Submit</Button>
+                            </Form>
+                        )
+                    }
+                    }
+                </Formik>
+            </div>
         </>
-    </>
-  );
+    );
 }
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,9 +3,9 @@ import * as yup from "yup"
 const formSchema = yup.object().shape({
     make: yup.string().min(1).max(25).required("Required"),
     model: yup.string().min(1).max(25).required("Required"),
-    yearOfMade: yup.number().positive().integer().required("Required"),
-    bodyType: yup.string().required("Required"),
-    fuelType: yup.string().required("Required"),
+    yearOfMade: yup.number().positive().integer("Only numbers").min(1950).max(2023).required("Required"),
+    bodyType: yup.string().oneOf(["sedan","wagon"]).required("Required"),
+    fuelType: yup.string().oneOf(["gasoline","diesel"]).required("Required"),
     licencePlate: yup.string().min(1).max(15).required("Required"),
 })
 export default formSchema
